396230a590c87d41f3bc0a69004f751b7b03d75e
81angelHELP98 <vangel338@gmail.com>
2024-01-09 20:25:17 -0300

    asgrego navbar y cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,30 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.min.js"
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import NavBar from "./components/navbar/NavBar.jsx"
+import CartWidget from "./components/navbar/CartWidget.jsx"
 
 function App() {
-  const [count, setCount] = useState(0)
+  //const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Nuevo E-Commerce</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <body className="container-fluid">
+        <header className="box-NavBar">
+          <NavBar className="navBar"/>
+          <CartWidget/>
+        </header>
+        <div className="container-fluid">
+          <div className="row">
+          <main>
+            <section className="">
+              <button type="button" className="btn btn-primary">Primary</button>
+            </section>
+          </main>
+          </div>
+        </div>
+      </body>
     </>
   )
 }
