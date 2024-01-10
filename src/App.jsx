d955e34630284f5fe3154fe5cfd99903b653c51a
81angelHELP98 <vangel338@gmail.com>
2024-01-10 07:26:50 -0300

    add count element

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,10 @@ import "bootstrap/dist/js/bootstrap.min.js"
 import { useState } from 'react'
 import './App.css'
 import NavBar from "./components/navbar/NavBar.jsx"
-import CartWidget from "./components/navbar/CartWidget.jsx"
+import CartWidget from "./components/cart/CartWidget.jsx"
 
 function App() {
-  //const [count, setCount] = useState(0)
-
+  
   return (
     <>
       <body className="container-fluid">
@@ -19,7 +18,7 @@ function App() {
           <div className="row">
           <main>
             <section className="">
-              <button type="button" className="btn btn-primary">Primary</button>
+              
             </section>
           </main>
           </div>
--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -6,25 +6,29 @@ import '../../App.css'
 
 function NavBar() {
     return (
-        <>
-            <nav className="navbar navbar-expand-lg bg-body-tertiary">
-                <div className="container-fluid">
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarNav">
-                        <ul className="navbar-nav">
-                            <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="#">Home</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">Features</a>
-                            </li>
-                        </ul>
-                    </div>
+
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <div className="container-fluid">
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Features</a>
+                        </li>
+                    </ul>
+                    <form className="d-flex" role="search">
+                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                        <button className="btn btn-outline-success" type="submit">Search</button>
+                    </form>
                 </div>
-            </nav>
-        </>
+            </div>
+        </nav>
+
     )
 }
 
